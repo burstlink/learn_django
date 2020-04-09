2ca2f238a373b7c1aba78b1307160d986213f359
Leyy <liyy453180905@gmail.com>
2020-04-10 01:01:47 +0800

    update200410

--- mysite/polls/urls.py
+++ mysite/polls/urls.py
@@ -5,4 +5,8 @@ from . import views
 # route 网址路径 view 视图函数 kwargs name 模块调用别名
 urlpatterns = [
     path('', views.index, name='index'),
+    path('<int:question_id>/', views.detail, name='detail'),
+    path('<int:question_id>/results/', views.detail, name='detail'),
+    path('<int:question_id>/vote/', views.detail, name='detail'),
+
 ]
