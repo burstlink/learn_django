6e3a7e9bd5a901401e1c32004cf0e5fb6e5d247b
Li <liyy.fnst@fujitsu.com>
2020-04-10 17:54:18 +0800

    update200410_2

--- mysite/polls/urls.py
+++ mysite/polls/urls.py
@@ -3,10 +3,11 @@ from . import views
 
 # path 四个参数：
 # route 网址路径 view 视图函数 kwargs name 模块调用别名
+app_name = 'polls'
 urlpatterns = [
     path('', views.index, name='index'),
     path('<int:question_id>/', views.detail, name='detail'),
     path('<int:question_id>/results/', views.detail, name='detail'),
-    path('<int:question_id>/vote/', views.detail, name='detail'),
+    path('<int:question_id>/vote/', views.detail, name='vote'),
 
 ]
